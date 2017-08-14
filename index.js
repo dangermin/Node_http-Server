4b54ee54549cd85eccadd9b9d37b00eb3fa3e0bb
Dangermin <dangermin.field@gamil.com>
2017-08-14 15:14:49 -0700

    basic node server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// write your code here
+var http = require('http');
+http.createServer(function(req, res){
+  res.writeHead(200, {'content-type': 'text/plain'});
+  res.end('Hello World');
+}).listen(8080, '127.0.0.1');
+console.log('Server is listening on localhost:8080/');
